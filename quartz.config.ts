0f64a8d835614368214307b8d8ce2b9f6a747b1f
maxkuzzmenko <127974451+maxkuzzmenko@users.noreply.github.com>
2026-02-23 17:11:17 +0100

    Quartz sync: Feb 23, 2026, 5:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Amazon.com, Inc.",
+    pageTitle: "Amazon.com",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
